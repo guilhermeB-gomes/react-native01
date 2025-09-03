d3f4a65ec2ab037831be8f55cce50647b6cb10ef
saov4000 <saov4000@gmail.com>
2025-09-03 07:42:40 -0300

    first

--- App.tsx
+++ App.tsx
@@ -1,11 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [valor1, setValor1] = useState("");
+  const [valor2, setValor2] = useState("");
+  const [resultado, setResultado] = useState(0);
+
+  const somar = () => {
+    const soma = parseFloat(valor1) + parseFloat(valor2);
+    setResultado(soma);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.texto}>Valor 1</Text>
+      <TextInput value={valor1} onChangeText={setValor1} style={styles.input}/>
+      <Text style={styles.texto}>Valor 2</Text>
+      <TextInput value={valor2} onChangeText={setValor2} style={styles.input}/>
+      <Button title="CALCULAR" onPress={somar}/>
+      <Text style={styles.texto}>{resultado}</Text>
     </View>
   );
 }
@@ -16,5 +30,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  texto:{
+    fontSize:20,
+  },
+  input:{
+    borderWidth: 1,
+    borderColor: "#000",
+    padding: 8,
+    marginBottom: 10,
+    width: "80%",
+    textAlign: "center",
   },
 });
